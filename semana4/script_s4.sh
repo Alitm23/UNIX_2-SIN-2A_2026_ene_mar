aec80f71ad6394f9958ef634c50d066cdaba0b5c
Nataly Tituaña <natituanamo@uide.edu.ec>
2026-01-27 01:14:38 +0000

    feat: agregar archivos y script para la semana 4

---
+++ semana4/script_s4.sh
@@ -0,0 +1,10 @@
+mkdir -p padre_nt/hijo_nt
+pwd
+cd
+pwd
+cd /workspaces/UNIX_2-SIN-2A_2026_ene_mar/semana4/padre_nt/hijo_nt
+touch archivo_nt.txt
+echo '0' > archivo_nt.txt
+echo '1' >> archivo_nt.txt
+echo '2' >> archivo_nt.txt
+echo '3' >> archivo_nt.txt
